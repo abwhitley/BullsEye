f1fd3f08b0027a17c0b9347597bc7d0d1ef2ea97
abwhitley <a.blakewhitley@gmail.com>
2017-05-19 12:17:25 -0400

    Added web view for info screen

--- Bullseye/AboutViewController.swift
+++ Bullseye/AboutViewController.swift
@@ -13,9 +13,17 @@ class AboutViewController: UIViewController {
     @IBAction func close(){
         dismiss(animated: true, completion: nil)
     }
+    @IBOutlet weak var webView: UIWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let url = Bundle.main.url(forResource: "BullsEye", withExtension: "html"){
+            if let htmlData = try? Data(contentsOf: url){
+                let baseUrl = URL(fileURLWithPath: Bundle.main.bundlePath)
+                webView.load(htmlData, mimeType: "text/html", textEncodingName: "UTF-8", baseURL: baseUrl)
+            }
+        }
 
         // Do any additional setup after loading the view.
     }
